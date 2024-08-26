4f9be8ed111af67e387809193efdcf20739d95de
svinstvo-og <maxim12.05.2006koval@gmail.com>
2024-08-26 18:19:20 +0200

    Created db

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from website import create_app
+from website import create_app, hi
 
 app = create_app()
 
--- website/models.py
+++ website/models.py
@@ -1,2 +1,8 @@
-from . import db
+from __init__ import db
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    pwd = db.Column(db.String(32), unique=False, nullable=False)
+
+db.create_all()
